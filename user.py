7a2dfdd5bb932cf7570af62521443d1b4321088f
Dicky Halim <dicky.halim@traveloka.com>
2024-06-19 17:03:52 +0700

    fix: cleanup

--- user.py
+++ user.py
@@ -6,20 +6,5 @@ class User(UserMixin):
         self.username = username
         self.password = password
 
-    def get_id(self):
-        return self.username
-    
     def verify_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
-
-    @property
-    def is_active(self):
-        return True
-
-    @property
-    def is_authenticated(self):
-        return True
-
-    @property
-    def is_anonymous(self):
-        return False
