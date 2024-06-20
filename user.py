70a1dba3d38f09d38a9b65961d4d636d42b88d0e
Dicky Halim <dicky.halim@traveloka.com>
2024-06-20 16:36:28 +0700

    fix

--- user.py
+++ user.py
@@ -1,10 +1,26 @@
 from flask_login import UserMixin
 import bcrypt
 
+
 class User(UserMixin):
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def get_id(self):
+        return self.username
+
     def verify_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
